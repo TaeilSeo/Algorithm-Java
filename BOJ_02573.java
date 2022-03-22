f642eaafbc7d6400bd4f11279e0f8b8e55f0c2d1
TaeilSeo <100591243+TaeilSeo@users.noreply.github.com>
2022-03-22 18:56:07 +0900

    Update BOJ_02573.java

--- BOJ_02573.java
+++ BOJ_02573.java
@@ -9,11 +9,15 @@ import java.util.Scanner;
 public class Main {
 	static int R, C;
 	static int[][] grid;
+	static int[][] _grid;
 	static ArrayList<int[]> list;
 	static int res;
 	
 	static int[] dr = {0, -1, 0, 1};
 	static int[] dc = {-1, 0, 1, 0};
+	
+	static boolean[][] ch;
+	static int size, cnt;
 	
 	
 	public static void main(String[] args) throws Exception {
@@ -23,28 +27,16 @@ public class Main {
 		R = sc.nextInt();
 		C = sc.nextInt();
 		grid = new int[R][C];
+		_grid = new int[R][C];
 		list = new ArrayList<>();
 		res = 0;
 		
 		for(int r=0; r<R; r++) {
 			for(int c=0; c<C; c++) {
-				grid[r][c] = sc.nextInt();
+				int v = sc.nextInt();
+				grid[r][c] = v;
 				
-				if( grid[r][c] == 0 ) {
-					continue;
-				}
-				
-				int temp = 0;
-				for(int d=0; d<4; d++) {
-					int nr = r + dr[d];
-					int nc = c + dc[d];
-					
-					if(grid[nr][nc] != 0) temp++;
-				}
-				
-				if( temp != 4 ) {
-					list.add(new int[] {r, c});
-				}
+				if(grid[r][c] != 0) list.add(new int[] {r, c});
 			}
 		}
 		
@@ -57,8 +49,9 @@ public class Main {
 		
 		for(int i=list.size()-1; i>=0; i--) {
 			q.offer(list.get(i));
-			list.remove(i);
 		}
+		
+		if( check() ) return;
 		
 		while( !q.isEmpty() ) {
 			int size = q.size();
@@ -76,9 +69,9 @@ public class Main {
 					if(grid[nr][nc] == 0) temp++;
 				}
 
-				grid[r][c] = grid[r][c] - temp >= 0 ? grid[r][c] - temp : 0;
+				_grid[r][c] = grid[r][c] - temp >= 0 ? grid[r][c] - temp : 0;
 				
-				if(grid[r][c] > 0) {
+				if( _grid[r][c] > 0 ) {
 					int[] pos = {r, c};
 					q.offer(pos);
 					list.add(pos);
@@ -86,67 +79,58 @@ public class Main {
 			}
 			
 			for(int r=0; r<R; r++) {
-				System.out.println(Arrays.toString(grid[r]));
+				grid[r] = _grid[r].clone();
 			}
-			System.out.println();
+			res++;
 			
 			if( check() ) {
 				break;
 			}
-			
-			res++;
 		}
 	}
 	
 	public static boolean check() {
-		boolean res = true;
+		boolean flag = true;
+		ch = new boolean[R][C];
 		
-		Queue<int[]> q = new LinkedList<>();
-		boolean[][] ch = new boolean[R][C];
+		size = list.size();
+		if (size == 0) {
+			res = 0;
+			return flag;
+		};
+		
+		cnt = 0;
+		int[] start = list.get(0);
 		
 		for(int i=list.size()-1; i>=0; i--) {
 			int[] pos = list.get(i);
-			q.offer(pos);
 			ch[pos[0]][pos[1]] = true;
 			list.remove(i);
 		}
 		
-		for(int r=0; r<R; r++) {
-			System.out.println(Arrays.toString(ch[r]));
-		}
-		System.out.println();
+		ch[start[0]][start[1]] = false;
+		dfs(start[0], start[1]);
 		
-		int size = q.size();
-		
-		int cnt = 0;
-		
-		while( !q.isEmpty() ) {
-			int[] cur = q.poll();
-			int r = cur[0];
-			int c = cur[1];
-			
-			ch[r][c] = false;
-			
-			for(int d=0; d<4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				
-				if( ch[nr][nc] ) {
-					cnt++;
-					q.offer(new int[] {nr, nc});
-				}
-			}
-		}
-		
-		for(int r=0; r<R; r++) {
-			System.out.println(Arrays.toString(ch[r]));
-		}
-		
-		System.out.println("size: " + size + ", cnt : " + cnt);
 		
 		// 숫자가 같으면, 아직 빙산은 한 덩어리
-		if(size == cnt) res = false;
+		if(size == cnt) flag = false;
 		
-		return res;
+		return flag;
+	}
+	
+	
+	
+	public static void dfs(int r, int c) {
+		cnt++;
+		
+		for(int d=0; d<4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if( ch[nr][nc] ) {
+				ch[nr][nc] = false;
+				dfs(nr, nc);
+			}
+		}
 	}
 }
