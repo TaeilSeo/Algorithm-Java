079b39bf3acbe8cd6fabdb51ad2789feb14421fd
TaeilSeo <100591243+TaeilSeo@users.noreply.github.com>
2022-05-04 13:56:01 +0900

    Update BOJ_15686.java

--- BOJ_15686.java
+++ BOJ_15686.java
@@ -1,8 +1,8 @@
-package baekjoon15686_0502;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /*
@@ -20,40 +20,75 @@ import java.util.Scanner;
 public class Main {
 	static int N, M;
 	static int[][] grid;
+	static boolean[][] ch;
 	
-	static int[] dr = {};
-	static int[] dc = {};
+	static int[] dr = {0, 0, 1, -1};
+	static int[] dc = {1, -1, 0, 0};
+	
+	static ArrayList<Data> home;
+	static ArrayList<Data> chicken;
 	
 	static int answer;
 	
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream(new File(Main.class.getResource("").getPath() + "input.txt")));
 		Scanner sc = new Scanner(System.in);
 
 		N = sc.nextInt();
 		M = sc.nextInt();
 		grid = new int[N+2][N+2];
-		answer = 0;
+		answer = Integer.MAX_VALUE;
 		
-		ArrayList<Data> list = new ArrayList<>(); 
+		home = new ArrayList<>();
+		chicken = new ArrayList<>();
 		
 		for(int i=1; i<=N; i++) {
 			for(int j=1; j<=N; j++) {
 				grid[i][j] = sc.nextInt();
-				if(grid[i][j] == 1) list.add(new Data(i, j));
+				if(grid[i][j] == 1) home.add(new Data(i, j));
+				if(grid[i][j] == 2) chicken.add(new Data(i, j));
 			}
 		}
 		
+		// 치킨집을 최대 M개 선택할 때, 도시의 치킨거리 최솟값
+		for(int i=1; i<=M; i++) {
+			Data[] arr = new Data[i];
+			comb(0, 1, chicken.size(), i, arr);
+		}
 		
+		System.out.println(answer);
+	}
+	
+	// 선택 가능한 치킨집 수 
+	private static void comb(int L, int start, int n, int r, Data[] arr) {
+		if(L == r) {
+			getChickDist(arr);
+			return;
+		}
 		
+		for(int i=start; i<=n; i++) {
+			arr[L] = chicken.get(i-1);
+			comb(L+1, i+1, n, r, arr);
+		}
 	}
 
-	private static int bfs(int r, int c) {
-		int dist = 0;
+	private static void getChickDist(Data[] arr) {
+		int sum = 0;
 		
+		for(int i=0; i<home.size(); i++) {
+			int minDist = Integer.MAX_VALUE;
+			
+			for(int j=0; j<arr.length; j++) {
+				minDist = Integer.min(minDist, getDist(home.get(i), arr[j]));
+			}
+			
+			sum += minDist;
+		}
 		
-		
-		return dist;
+		answer = Integer.min(answer, sum);
+	}
+	
+	private static int getDist(Data pos1, Data pos2) {
+		return Math.abs(pos1.r - pos2.r) + Math.abs(pos1.c - pos2.c);
 	}
 	
 	public static class Data {
